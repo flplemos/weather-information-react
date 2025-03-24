a2c05c8a838034487d3a0fb23267598a220bced0
Felipe Lemos <139652554+flplemos@users.noreply.github.com>
2025-03-24 18:47:24 -0300

    Weather information 5 days
    Add weather information 5 days forecast

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,24 @@ import { useState, useRef } from 'react'
 import axios from 'axios'
 import './App.css'
 import WeatherInformations from './components/WeatherInformations/WeatherInformations'
+import WeatherInformations5Days from './components/WeatherInformations5Days/WeatherInformations5Days'
 
 function App() {
   const [weather, setWeather] = useState()
+  const [weather5Days, setWeather5Days] = useState()
   const inputRef = useRef()
 
   async function searchCity(){
     const city = inputRef.current.value
     const apiKey = '200d7c999fe95bfea1bbaf206acfbccc'
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&lang=pt_br&units=metric`
+    const url5Days = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&lang=pt_br&units=metric`
 
     const apiInfo = await axios.get(url)
+    const apiInfo5Days = await axios.get(url5Days)
+
     setWeather(apiInfo.data)
-  
+    setWeather5Days(apiInfo5Days.data)
   }
 
   return (
@@ -24,6 +29,7 @@ function App() {
       <button onClick={searchCity}>Buscar</button>
 
       {weather && <WeatherInformations weather={weather}/>}
+      {weather5Days && <WeatherInformations5Days weather5Days={weather5Days} />}
     </div>
   )
 }
--- src/components/WeatherInformations/WeatherInformations.jsx
+++ src/components/WeatherInformations/WeatherInformations.jsx
@@ -1,20 +1,22 @@
+import './WeatherInformations.css'
+
 function WeatherInformations({ weather }) {
     if (!weather.weather) {
       return <p>Busque por uma cidade para ver a previsão.</p>;
     }
   
     return (
-      <div>
+      <div className='weather-container'>
         <h2>{weather.name}</h2>
-        <div>
+        <div className='weahter-info'>
           <img
             src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}.png`}
             alt="Ícone do clima"
           />
-          <p>Temperatura: {Math.round(weather.main.temp)}°C</p>
+          <p className='temperature'>{Math.round(weather.main.temp)}°C</p>
         </div>
-        <p>{weather.weather[0].description}</p>
-        <div>
+        <p className='description'>{weather.weather[0].description}</p>
+        <div className='details'>
           <p>Sensação térmica: {Math.round(weather.main.feels_like)}°C</p>
           <p>Umidade: {weather.main.humidity}</p>
           <p>Pressão: {weather.main.pressure}</p>
